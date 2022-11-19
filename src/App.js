6f000f88debdfb887807d7416387704215e0da24
unknown <artem.miniuk@gmail.com>
2022-11-19 03:05:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/Input";
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App(props) {
+  const [colors, setColors] = useState(["red", "blue", "purple"]);
+  const [text, setText] = useState();
+
+  const changeText = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleOnClick = (event) => {
+    let newArray = [...colors, text];
+    setColors(newArray);
+  };
+
+  useEffect(() => {
+    window.location.replace(`http://localhost:3000/#tags=${colors}`);
+  }, [colors]);
+
+  const deleteItem = (index) => {
+    let newArray = colors.filter((item, i) => {
+      return i !== index;
+    });
+    setColors(newArray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="menu">
+      <div>
+        <h1>Colors</h1>
+        {colors.map((item, index) => (
+          <ul>
+            <li onClick={() => deleteItem(index)} key={index}>
+              {item}
+            </li>
+          </ul>
+        ))}
+      </div>
+      <div className="menu_add_new_item">
+        <div>
+          <TextField
+            value={text}
+            onChange={changeText}
+            id="outlined-basic"
+            variant="outlined"
+          />
+        </div>
+        <div>
+          <Button onClick={handleOnClick} variant="contained">
+            Add
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
